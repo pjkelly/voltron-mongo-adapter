df8bdc0ec271d0b5ce9781f673b2e9a5b0ed884b
Justin Reidy <jmreidy@rzrsharp.net>
2012-11-08 17:30:31 -0800

    completely refactoring how the postgres adapter works

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ VoltronAdapter.Mongo = require('./lib/adapters/mongo');
 VoltronAdapter.Postgres = require('./lib/adapters/postgres');
 
 function VoltronAdapter(Model, adapter, hooks) {
+  adapter.modelConstructor = Model;
+  adapter.primaryKey = Model.prototype._primaryKey;
+
   var Adapter = adapter.constructor;
+
   Adapter.addStatics(Model, adapter);
   Adapter.addDelegates(Model, adapter);
 
